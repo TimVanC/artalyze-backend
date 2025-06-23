612e07cc7150c21ae7f6130eb467f2226e3fb3ac
TimVanC <timvancau@gmail.com>
2025-06-23 02:01:32 -0400

    Add database connection logging to identify correct database name

--- config/db.js
+++ config/db.js
@@ -4,8 +4,14 @@ require('dotenv').config();
 
 const username = process.env.MONGO_USER;
 const password = process.env.MONGO_PASS;
-const dbName = process.env.NODE_ENV === "staging" ? "artalyze_staging" : "artalyze";
+const dbName = process.env.NODE_ENV === "staging" ? "artalyze_staging" : process.env.MONGO_DB_NAME || "artalyze";
 const uri = `mongodb+srv://${username}:${password}@cluster0.rr4c6.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+console.log('Database connection details:');
+console.log('- NODE_ENV:', process.env.NODE_ENV);
+console.log('- MONGO_DB_NAME env var:', process.env.MONGO_DB_NAME);
+console.log('- Selected dbName:', dbName);
+console.log('- Full URI (without password):', uri.replace(password, '***'));
 
 const connectDB = async () => {
     try {
